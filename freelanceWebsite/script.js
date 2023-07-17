a77de8230647483be9c3a568737e6fac319de98a
justcodess <zeynepdeler@gmail.com>
2023-07-17 13:19:03 +0300

    commit

---
+++ freelanceWebsite/script.js
@@ -0,0 +1,11 @@
+const search=document.querySelector("#search");
+const searchform=document.querySelector("#searchform");
+
+search.addEventListener("click",function(){
+    searchform.classList.toggle("active");
+    // document.addEventListener("click",function(){
+    //     if(e.composedPath().includes(search)){
+
+    //     }
+    // })
+});
